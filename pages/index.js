39663ed5165065483bbb7475d96334bc4fca2857
yaho48 <118334102+yaho48@users.noreply.github.com>
2022-11-16 10:12:15 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -16,37 +16,10 @@ export default function Home() {
           <a href="https://nextjs.org">YAHO48!</a>
         </h1>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}></a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}></a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          ></a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.card}
-          ></a>
-        </div>
+        <div className={styles.grid}></div>
       </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+      <footer className={styles.footer}></footer>
     </div>
   );
 }
